31f78b466fb64e7c5c0c558457611fb4d1d61213
Dai Nguyen Trong <dai.nguyentrong@nashtechglobal.com>
2023-02-27 14:42:45 +0700

    Setup project

---
+++ src/main.php
@@ -0,0 +1,18 @@
+<?php
+
+require_once 'vendor/autoload.php';
+
+class Main
+{
+    public function __construct()
+    {
+        $this->run();
+    }
+
+    public function run()
+    {
+        // Code here
+    }
+}
+
+$main = new Main();
